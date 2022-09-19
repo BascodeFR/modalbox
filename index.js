e7a837383d51b964131e1efcfa3031d68dc02e3b
Cavernos <louisdescavernes@gmail.com>
2022-09-19 21:43:14 +0200

    Add First Export

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const openModal  = (e) => {
+export const openModal  = (e) => {
     e.preventDefault()
     e.target.getAttribute('href')
 }
-
-document.querySelectorAll('.js-modal').forEach(a => {
-    a.addEventListener('click', openModal)
-})
